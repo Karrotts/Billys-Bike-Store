baebba5355d1c4739327c285e0be3f994e4ebb92
wes <Wesleymiller1996@gmail.com>
2021-03-31 13:35:26 -0500

    added navigation component

--- BikeProducts.Tests/HomeControllerTests.cs
+++ BikeProducts.Tests/HomeControllerTests.cs
@@ -5,6 +5,7 @@ using Moq;
 using BikeProducts.Controllers;
 using BikeProducts.Models;
 using Xunit;
+using BikeProducts.Models.ViewModels;
 
 namespace SportsStore.Tests
 {
@@ -15,19 +16,18 @@ namespace SportsStore.Tests
         {
             // Arrange
             Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
-
             mock.Setup(m => m.Products).Returns((new Product[] {
                 new Product {ProductID = 1, Name = "P1"},
                 new Product {ProductID = 2, Name = "P2"}
-            }).AsQueryable<Product>());
-
+                }).AsQueryable<Product>());
             HomeController controller = new HomeController(mock.Object);
+
             // Act
-            IEnumerable<Product> result =
-            (controller.Index() as ViewResult).ViewData.Model
-            as IEnumerable<Product>;
+            ProductsListViewModel result =
+                controller.Index(null).ViewData.Model as ProductsListViewModel;
+            
             // Assert
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.True(prodArray.Length == 2);
             Assert.Equal("P1", prodArray[0].Name);
             Assert.Equal("P2", prodArray[1].Name);
@@ -49,15 +49,69 @@ namespace SportsStore.Tests
             controller.PageSize = 3;
 
             // Act
-            IEnumerable<Product> result =
-                (controller.Index(2) as ViewResult).ViewData.Model
-                as IEnumerable<Product>;
+            ProductsListViewModel result =
+                controller.Index(null, 2).ViewData.Model as ProductsListViewModel;
 
             // Assert
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.True(prodArray.Length == 2);
             Assert.Equal("P4", prodArray[0].Name);
             Assert.Equal("P5", prodArray[1].Name);
+        }
+
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+                }).AsQueryable<Product>());
+
+            // Arrange
+            HomeController controller =
+                new HomeController(mock.Object) { PageSize = 3 };
+
+            // Act
+            ProductsListViewModel result =
+                controller.Index(null, 2).ViewData.Model as ProductsListViewModel;
+
+            // Assert
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(3, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.TotalItems);
+            Assert.Equal(2, pageInfo.TotalPages);
+        }
+
+        [Fact]
+        public void Can_Filter_Products()
+        {
+            // Arrange
+            // - create the mock repository
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+                new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+                new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+                new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+                }).AsQueryable<Product>());
+            // Arrange - create a controller and make the page size 3 items
+            HomeController controller = new HomeController(mock.Object);
+            controller.PageSize = 3;
+            // Action
+            Product[] result =
+            (controller.Index("Cat2", 1).ViewData.Model as ProductsListViewModel)
+            .Products.ToArray();
+            // Assert
+            Assert.Equal(2, result.Length);
+            Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
         }
     }
 }
--- BikeProducts/Infrastructure/PageLinkTagHelper.cs
+++ BikeProducts/Infrastructure/PageLinkTagHelper.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using BikeProducts.Models.ViewModels;
+using System.Collections.Generic;
 
 namespace BikeProducts.Infrastructure
 {
@@ -20,6 +21,9 @@ namespace BikeProducts.Infrastructure
         public ViewContext ViewContext { get; set; }
         public PagingInfo PageModel { get; set; }
         public string PageAction { get; set; }
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> PageUrlValues { get; set; }
+            = new Dictionary<string, object>();
         public bool PageClassesEnabled { get; set; } = false;
         public string PageClass { get; set; }
         public string PageClassNormal { get; set; }
@@ -31,9 +35,8 @@ namespace BikeProducts.Infrastructure
             for (int i = 1; i <= PageModel.TotalPages; i++)
             {
                 TagBuilder tag = new TagBuilder("a");
-
-                tag.Attributes["href"] = urlHelper.Action(PageAction,
-                    new { productPage = i });
+                PageUrlValues["productPage"] = i;
+                tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
 
                 if (PageClassesEnabled)
                 {
